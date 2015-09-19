74995dde802586a64a97ddb951073166a2c94b73
Bateelsaber <sabeb234@newschool.edu>
2015-09-19 23:14:20 +0300

    challenge 4 coding

--- challenge_4/sketch.js
+++ challenge_4/sketch.js
@@ -1,18 +1,66 @@
+var ballX = 200;
+var ballY = 100;
+var ballSpeedX = 4;
+var ballSpeedY = 4;
+var ballXa = 100;
+var ballYa = 100;
+var ballSpeedXa = 4;
+var ballSpeedYa = 4;
+
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
 	noStroke();
-	noLoop();
 }
 
 function draw() {
-	// clear the background
-	background(150, 50, 50);
 
-	// set a fill color
-	fill(255, 255, 255);
+	//////////////////////////////
+	// update
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	// change position
+	ballX = ballX + ballSpeedX;
+	ballY = ballY + ballSpeedY;
+	ballXa = ballXa + ballSpeedXa;
+	ballYa = ballYa + ballSpeedYa;
+
+
+	// don't let the ball go to far
+
+	// right
+	if (ballX > width) {
+		ballSpeedX = -ballSpeedX;
+	} else {
+		ballSpeedXa = -ballSpeedX;
+
+	}
+
+	// bottom
+	if (ballY > height) {
+		ballSpeedY = -ballSpeedY;
+	} else {
+		ballSpeedYa = ballSpeedY;
+	}
+
+	// left
+	if (ballX < 0) {
+		ballSpeedX = -ballSpeedX;
+	} else {
+		ballSpeedXa = -ballSpeedX;
+	}
+
+	// top
+	if (ballY < 0) {
+		ballSpeedY = -ballSpeedY;
+	} else {
+		ballSpeedYa = -ballSpeedY;
+	}
+
+	//////////////////////////////
+	// draw
+
+	background(0);
+	ellipse(ballX, ballY, 50, 50);
+	ellipse(ballXa + 300, ballYa + 150, 50, 50);
+
 }
